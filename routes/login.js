e45aaefd19be392e4185af32c5f759046a45905c
cinnamonbagels <samuelsko@gmail.com>
2015-02-24 19:37:03 -0800

    fixed login

--- routes/login.js
+++ routes/login.js
@@ -14,14 +14,14 @@ exports.validateLogin = function(req, res) {
 	var fields = req.body;
 
 	User.findOne({ email : fields.email }, function(err, data) {
-		if(data.email === fields.email) {
+		if(data) {
 			if(data.password === fields.password) {
 				req.session.email = data.email;
 				res.redirect('/classes');
 			} else {
 				res.send({
 					err : 'Invalid Password'
-				})
+				});
 			}
 		} else {
 			var output = 'User ' + fields.email + ' does not exist, Register first!'
